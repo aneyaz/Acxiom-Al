13d9825ecdef1f4d783ada11271c85e70daeddd4
Rahul Kandu <rahulmpic.gkp@gmail.com>
2023-09-18 20:57:05 +0530

    assign

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GuestList from './components/GuestList';
+import OrderStatus from './components/OrderStatus';
+
+function App() {
+  return (
+
+  );
+}
+
+export default App;
